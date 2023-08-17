81695593f9ef61f1e8d881cec06565c6fc1432a4
pdiksha30 <pateldixa30@gmail.com>
2023-08-17 13:02:04 -0400

    updated package.json

---
+++ Server/Config/db.ts
@@ -0,0 +1,9 @@
+let localURI = 'mongodb://127.0.0.1:27017/movies';
+let remoteURI = 'mongodb+srv://thomas:VENcSV4FZZzTOvMR@moviecluster.yf0nclt.mongodb.net/movies';
+let secret = 'SomeSecret';
+
+export default {
+    localURI: localURI,
+    remoteURI: remoteURI,
+    secret: secret
+}
